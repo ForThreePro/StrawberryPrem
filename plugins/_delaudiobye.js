fa3bd756de784728d93924aadbc4ae6ec7080cef
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 08:52:48 -0500

    Crear _delaudiobye.js

---
+++ plugins/_delaudiobye.js
@@ -0,0 +1,20 @@
+let handler = async (m, { conn, isAdmin }) => {
+  if (!isAdmin) return m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
+*│* 💕 *SOLO ADMINS*
+*╰────────────────────🌸*`);
+
+  let chat = global.db.data.chats[m.chat]
+  if (!chat) global.db.data.chats[m.chat] = {}
+  chat.byeAudio = null
+  await global.db.write()
+
+  m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
+*│* ✅ *AUDIO DE DESPEDIDA ELIMINADO*
+*╰────────────────────🌸*`);
+}
+handler.help = ['delaudiobye']
+handler.tags = ['group']
+handler.command = ['delaudiobye']
+handler.admin = true
+handler.group = true
+export default handler
