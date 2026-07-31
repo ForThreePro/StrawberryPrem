8c3b513ea74d4b01024888859b02e8a1c460b86e
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 11:47:14 -0500

    Actualizar setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -1,28 +1,17 @@
 let handler = async (m, { conn, isAdmin, text }) => {
-  if (!isAdmin) return m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 💕 *SOLO ADMINS PUEDEN USAR ESTO*
-*╰────────────────────🌸*`);
-  if (!text) return m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* ✨ *ESCRIBE UN TEXTO LINDO*
-*│*
-*│* *Ejemplo*:.setwelcome Bienvenida @name a @group
-*│*
-*│* *Variables*: @name @user @group @desc %users @action @date
-*╰────────────────────🌸*`);
+  if (!isAdmin) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 😡 *SOLO ADMINS*\n╰─────────────────🍓`;
+  if (!text) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ ⚠️ *FALTA EL TEXTO*\n│\n│ *Ejemplo*:.setwelcome @name bienvenid@ a @group\n│\n│ *Variables*: @name @user @group @desc %users @action @date\n╰─────────────────🍓`;
 
-  let chat = global.db.data.chats[m.chat]
-  if (!chat) global.db.data.chats[m.chat] = {}
+  let chat = global.db.data.chats[m.chat] || {}
   chat.welcomeText = text
+  global.db.data.chats[m.chat] = chat
   await global.db.write()
 
-  m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 🌷 *BIENVENIDA ACTUALIZADA*
-*│* *Ahora todo se ve más cute*
-*╰────────────────────🌸*`);
+  m.reply(`╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ ✅ *BIENVENIDA GUARDADA*\n╰─────────────────🍓`);
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
-handler.command = ['setwelcome']
+handler.command = /^setwelcome$/i
 handler.admin = true
 handler.group = true
 export default handler
