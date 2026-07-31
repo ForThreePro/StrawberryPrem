2404d0976d1d1230edd1acccdf5bbc1e82831739
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 11:58:33 -0500

    Actualizar del welcome.js

--- plugins/del welcome.js
+++ plugins/del welcome.js
@@ -1,14 +1,27 @@
-let handler = async (m, { isAdmin }) => {
-  if (!isAdmin) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 😡 *SOLO ADMINS*\n╰─────────────────🍓`;
-  let chat = global.db.data.chats[m.chat] || {}
-  chat.welcomeText = null
-  global.db.data.chats[m.chat] = chat
+let handler = async (m, { conn, isAdmin }) => {
+  if (!isAdmin) return m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
+│ 😡 *SIN PERMISOS*
+╰─────────────────🐉`);
+
+  let chat = global.db.data.chats[m.chat]
+  if (!chat) global.db.data.chats[m.chat] = {}
+  chat = global.db.data.chats[m.chat]
+
+  chat.welcomeText = null;
   await global.db.write()
-  m.reply(`╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 🗑️ *BIENVENIDA ELIMINADA*\n╰─────────────────🍓`);
+  m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
+│ ✅ *BIENVENIDA ELIMINADA*
+│
+│ *Volvio al mensaje DBZ por defecto*
+╰─────────────────🐉`);
 }
-handler.help = ['delwelcome']
-handler.tags = ['group']
-handler.command = /^delwelcome$/i
-handler.admin = true
-handler.group = true
-export default handler
+
+handler.help = ['delwelcome'];
+handler.tags = ['group'];
+handler.command = ['delwelcome'];
+handler.admin = true;
+handler.group = true;
+
+export default handler;
+
+Del welcome
