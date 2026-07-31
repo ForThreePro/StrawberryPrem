07ca75f4d43f5dcd29b62b2f08328f147a6d4643
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 11:49:21 -0500

    Actualizar del welcome.js

--- plugins/del welcome.js
+++ plugins/del welcome.js
@@ -1,21 +1,14 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (!isAdmin) return m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 💕 *SOLO ADMINS*
-*╰────────────────────🌸*`);
-
-  let chat = global.db.data.chats[m.chat]
-  if (!chat) global.db.data.chats[m.chat] = {}
+let handler = async (m, { isAdmin }) => {
+  if (!isAdmin) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 😡 *SOLO ADMINS*\n╰─────────────────🍓`;
+  let chat = global.db.data.chats[m.chat] || {}
   chat.welcomeText = null
+  global.db.data.chats[m.chat] = chat
   await global.db.write()
-
-  m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 🗑️ *BIENVENIDA ELIMINADA*
-*│* *Volví al estilo fresita por defecto*
-*╰────────────────────🌸*`);
+  m.reply(`╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 🗑️ *BIENVENIDA ELIMINADA*\n╰─────────────────🍓`);
 }
 handler.help = ['delwelcome']
 handler.tags = ['group']
-handler.command = ['delwelcome']
+handler.command = /^delwelcome$/i
 handler.admin = true
 handler.group = true
 export default handler
