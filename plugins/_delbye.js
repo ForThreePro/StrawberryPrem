58673cdd0737b0399029af0c0fa079e540fd0ed9
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 11:50:04 -0500

    Actualizar _delbye.js

--- plugins/_delbye.js
+++ plugins/_delbye.js
@@ -1,20 +1,14 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (!isAdmin) return m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 💕 *SOLO ADMINS*
-*╰────────────────────🌸*`);
-
-  let chat = global.db.data.chats[m.chat]
-  if (!chat) global.db.data.chats[m.chat] = {}
+let handler = async (m, { isAdmin }) => {
+  if (!isAdmin) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 😡 *SOLO ADMINS*\n╰─────────────────🍓`;
+  let chat = global.db.data.chats[m.chat] || {}
   chat.byeText = null
+  global.db.data.chats[m.chat] = chat
   await global.db.write()
-
-  m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 🗑️ *DESPEDIDA ELIMINADA*
-*╰────────────────────🌸*`);
+  m.reply(`╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 🗑️ *DESPEDIDA ELIMINADA*\n╰─────────────────🍓`);
 }
 handler.help = ['delbye']
 handler.tags = ['group']
-handler.command = ['delbye']
+handler.command = /^delbye$/i
 handler.admin = true
 handler.group = true
 export default handler
