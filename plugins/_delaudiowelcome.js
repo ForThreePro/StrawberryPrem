bac09dca6b3ca4f19fe33ade55d0a25b7d746bb1
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 11:50:50 -0500

    Actualizar _delaudiowelcome.js

--- plugins/_delaudiowelcome.js
+++ plugins/_delaudiowelcome.js
@@ -1,20 +1,12 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (!isAdmin) return m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* 💕 *SOLO ADMINS*
-*╰────────────────────🌸*`);
-
-  let chat = global.db.data.chats[m.chat]
-  if (!chat) global.db.data.chats[m.chat] = {}
+let handler = async (m, { isAdmin }) => {
+  if (!isAdmin) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 😡 *SOLO ADMINS*\n╰─────────────────🍓`;
+  let chat = global.db.data.chats[m.chat] || {}
   chat.welcomeAudio = null
+  global.db.data.chats[m.chat] = chat
   await global.db.write()
-
-  m.reply(`*╭─🍓『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』🍓─╮*
-*│* ✅ *AUDIO DE BIENVENIDA ELIMINADO*
-*╰────────────────────🌸*`);
+  m.reply(`╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ ✅ *AUDIO BIENVENIDA ELIMINADO*\n╰─────────────────🍓`);
 }
-handler.help = ['delaudiowelcome']
-handler.tags = ['group']
-handler.command = ['delaudiowelcome']
+handler.command = /^delaudiowelcome$/i
 handler.admin = true
 handler.group = true
 export default handler
