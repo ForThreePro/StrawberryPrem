000c77bf3979291d45ba4b51a29407bb3badf75c
YALLICO OWNER <yallico2024@gmail.com>
2026-07-31 11:59:22 -0500

    Actualizar _delaudiobye.js

--- plugins/_delaudiobye.js
+++ plugins/_delaudiobye.js
@@ -1,12 +1,21 @@
-let handler = async (m, { isAdmin }) => {
-  if (!isAdmin) throw `╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ 😡 *SOLO ADMINS*\n╰─────────────────🍓`;
-  let chat = global.db.data.chats[m.chat] || {}
+let handler = async (m, { conn, isAdmin }) => {
+  if (!isAdmin) return m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
+│ 😡 *SOLO ADMINS*
+╰─────────────────🐉`);
+
+  let chat = global.db.data.chats[m.chat]
+  if (!chat) global.db.data.chats[m.chat] = {}
+  chat = global.db.data.chats[m.chat]
+
   chat.byeAudio = null
-  global.db.data.chats[m.chat] = chat
   await global.db.write()
-  m.reply(`╭─🍓 *『 𝗦𝗧𝗥𝗔𝗪𝗕𝗘𝗥𝗥𝗬 𝗕𝗢𝗧 』* 🍓─╮\n│ ✅ *AUDIO DESPEDIDA ELIMINADO*\n╰─────────────────🍓`);
+  m.reply(`╭─🐉 *『 𝗦𝗢𝗡 𝗚𝗢𝗞𝗨 𝗣𝗥𝗘𝗠 』* 🐉─╮
+│ ✅ *AUDIO DE DESPEDIDA ELIMINADO*
+╰─────────────────🐉`);
 }
-handler.command = /^delaudiobye$/i
+handler.help = ['delaudiobye']
+handler.tags = ['group']
+handler.command = ['delaudiobye']
 handler.admin = true
 handler.group = true
 export default handler
